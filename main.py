58a4f0d297974ef7cf78dd9805843ac292289aab
kael <liaght01@gmail.com>
2025-12-07 01:00:23 -0800

    Initial Commit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from resource import MENU,resources
 
-# TODO: 1. print report of coffee machine resources
-
-# TODO: 2. ask what the user would like (espresso/latte/cappuccino)
 power_on = True
 while power_on:
     choice = input("What would you like? (espresso/latte/cappuccino): ")
@@ -11,11 +8,29 @@ while power_on:
             print(f"{key.title()}: {value}")
     elif choice == "off":
         power_on = False
+        break
     else:
+        print("Please insert coins.")
+        quarters = input("how many quarters?: ")
+        dimes = input("how many dimes?: ")
+        nickles = input("how many nickles?: ")
+        pennies = input("how many pennies?: ")
+        quarters = int(quarters) * 0.25
+        dimes = int(dimes) * 0.10
+        nickles = int(nickles) * 0.05
+        pennies = int(pennies) * 0.01
+        #print(quarters, dimes, nickles, pennies)
+        total = quarters + dimes + nickles + pennies
+        if total > MENU[choice]["cost"]:
+            total -= MENU[choice]["cost"]
+            print(f"Here is ${round(total, 2)} in change.")
+        else:
+            print("Sorry, you don't have enough money! Money refunded.")
+            break
         drink_type = MENU[choice]["ingredients"]
         sufficient = True
         for key, required in drink_type.items():
-            if required > resources.get(key,0):
+            if required > resources.get(key, 0):
                 print(f"Sorry, not enough {key}.")
                 sufficient = False
                 break
@@ -23,8 +38,4 @@ while power_on:
             for key, required in drink_type.items():
                 resources[key] -= required
             print(f"Enjoy your {choice}!")
-            print(resources)
-# TODO: 3. check if there's enough resources to create the users drink, if not exit and print not enough of x resource
-# TODO: 4. insert coins if there's enough resources. quarter 0.25, dime 0.10, nickle 0.05, penny 0.01
-# TODO: 5. check if enough coins were inserted to continue transaction
-# TODO: 6. If enough coins subtract ingredients required from resources and output requested drink.
+            # print(resources)
