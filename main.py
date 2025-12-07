ec1e302f1a8af584d39eb01eca0901c8e05a84c7
kael <liaght01@gmail.com>
2025-12-07 01:42:15 -0800

    refactored payment logic and drink selection into their own functions.

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
 from resource import MENU,resources
 
 power_on = True
+
+def insert_coins(drink):
+    while True:
+        print("Please insert coins.")
+        try:
+            quarters = int(input("how many quarters?: "))
+            dimes = int(input("how many dimes?: "))
+            nickles = int(input("how many nickles?: "))
+            pennies = int(input("how many pennies?: "))
+            total = quarters * 0.25 + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
+            if total > MENU[drink]["cost"]:
+                total -= MENU[drink]["cost"]
+                print(f"Here is ${round(total, 2)} in change.")
+                return True
+            else:
+                print("Sorry, you don't have enough money! Money refunded.")
+                return False
+        except ValueError:
+            print("Sorry, type numbers only.")
+
+def drink_select(drink):
+    drink_type = MENU[drink]["ingredients"]
+    sufficient = True
+    for keys, required in drink_type.items():
+        if required > resources.get(keys, 0):
+            print(f"Sorry, not enough {keys}.")
+            sufficient = False
+            break
+    can_afford = insert_coins(drink)
+    if sufficient and can_afford:
+        for keys, required in drink_type.items():
+            resources[keys] -= required
+        print(f"Enjoy your {drink}!")
+        # print(resources)
+
 while power_on:
     choice = input("What would you like? (espresso/latte/cappuccino): ")
     if choice == "report":
@@ -8,34 +43,5 @@ while power_on:
             print(f"{key.title()}: {value}")
     elif choice == "off":
         power_on = False
-        break
     else:
-        print("Please insert coins.")
-        quarters = input("how many quarters?: ")
-        dimes = input("how many dimes?: ")
-        nickles = input("how many nickles?: ")
-        pennies = input("how many pennies?: ")
-        quarters = int(quarters) * 0.25
-        dimes = int(dimes) * 0.10
-        nickles = int(nickles) * 0.05
-        pennies = int(pennies) * 0.01
-        #print(quarters, dimes, nickles, pennies)
-        total = quarters + dimes + nickles + pennies
-        if total > MENU[choice]["cost"]:
-            total -= MENU[choice]["cost"]
-            print(f"Here is ${round(total, 2)} in change.")
-        else:
-            print("Sorry, you don't have enough money! Money refunded.")
-            break
-        drink_type = MENU[choice]["ingredients"]
-        sufficient = True
-        for key, required in drink_type.items():
-            if required > resources.get(key, 0):
-                print(f"Sorry, not enough {key}.")
-                sufficient = False
-                break
-        if sufficient:
-            for key, required in drink_type.items():
-                resources[key] -= required
-            print(f"Enjoy your {choice}!")
-            # print(resources)
+        drink_select(choice)
